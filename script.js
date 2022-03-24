edf77a56d6c14112d50cf4a86b312ba6b2dd343e
Frank <dominickokovic92@gmail.com>
2022-03-25 00:38:29 +0100

    Add createNewBoard() and clearBoard() functions
    Added a clearBoard() function which removes the display board altogether
    and replaces it with a new empty <div> with the right id.
    createNewBoard() prompts the user for the desired size of a new board
    and generates one.
    
    Also added a button which allows the user to call createNewBoard() at
    will.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const board = document.getElementById('board');
+let board = document.getElementById('board');
+const controls = document.getElementById('controls');
+const frame = document.getElementById('frame');
 let boardSideLength = 16;
 
 function fillBoard(board, boardSideLength) {
@@ -31,3 +33,36 @@ function setPrimaryButtonState(e) {
 document.addEventListener("mousedown", setPrimaryButtonState);
 document.addEventListener("mousemove", setPrimaryButtonState);
 document.addEventListener("mouseup", setPrimaryButtonState);
+//End of the mouse button code.
+
+const buttonNewBoard = document.getElementById('clear-button');
+buttonNewBoard.addEventListener('click', createNewBoard);
+
+function createNewBoard() {
+  let userInput = prompt('How big would you like your new board to be?\n' +
+  'Please enter the desired side length below (no longer than 100) or ' +
+  'type "cancel" or hit Escape key if you wish to cancel.');
+  if (userInput === null || userInput === 'cancel') {
+    alert('Canceled creating a new board.');
+    return;
+  }
+  userInput = Number(userInput);
+  while (isNaN(userInput) || userInput > 100 || userInput < 1 || !(Number.isInteger(userInput))) {
+    userInput = prompt('Please enter a correct number from 1 to 100.\n' +
+    'Type "cancel" or hit Escape key to cancel.');
+    if (userInput === null || userInput === 'cancel') {
+      alert('Canceled creating a new board.');
+      return;
+    }
+    userInput = Number(userInput);
+  }
+  clearBoard();
+  return fillBoard(board, userInput);
+}
+
+function clearBoard() {
+  board.remove();
+  board = document.createElement('div');
+  board.id = 'board';
+  frame.insertBefore(board, controls); 
+}
