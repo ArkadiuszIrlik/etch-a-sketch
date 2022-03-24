d82dbf7cf9e2506ea745b51e12ab374d9e09778d
Frank <dominickokovic92@gmail.com>
2022-03-24 23:43:45 +0100

    Add an event listener to the board display
    Every div is given an event listener which changes its color while the
    mouse cursor is hovering over it and the primary mouse button is held
    down. This allows the user to draw on the board.
    
    Also added some auxiliary code to check if the mouse button is currently
    being held down.

--- script.js
+++ script.js
@@ -6,9 +6,28 @@ function fillBoard(board, boardSideLength) {
     const currentSquare = document.createElement('div');
     currentSquare.classList.add('board-square');
     currentSquare.style.flexBasis = `${40 / boardSideLength}vw`; //Sets the size of each square as a proportion of the viewport width, 40 being the width of the board
+    currentSquare.addEventListener('mouseover', colorSquare);
     board.appendChild(currentSquare);
   }
   return board;
 }
 
+function colorSquare() {
+  if (primaryMouseButtonDown) {
+    return this.classList.add('colored');
+  }
+}
+
 fillBoard(board, boardSideLength);
+
+//The following code checks if the primary mouse button is currently being held down.
+let primaryMouseButtonDown = false;
+
+function setPrimaryButtonState(e) {
+  var flags = e.buttons !== undefined ? e.buttons : e.which;
+  primaryMouseButtonDown = (flags & 1) === 1;
+}
+
+document.addEventListener("mousedown", setPrimaryButtonState);
+document.addEventListener("mousemove", setPrimaryButtonState);
+document.addEventListener("mouseup", setPrimaryButtonState);
