e0c0c1ca666cdd0c190365c2d1918551c8ccfd51
Frank <dominickokovic92@gmail.com>
2022-03-24 19:15:16 +0100

    Add fillBoard() function
    Added fillBoard() function, which fills the display board with a user
    defined number of squares (default of 16 per side). Also added CSS rules
    to the squares to give them the right color and an outline to
    distinguish individual squares.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 const board = document.getElementById('board');
+let boardSideLength = 16;
+
+function fillBoard(board, boardSideLength) {
+  for (let i = 0; i < (boardSideLength**2); i++) {
+    const currentSquare = document.createElement('div');
+    currentSquare.classList.add('board-square');
+    currentSquare.style.flexBasis = `${40 / boardSideLength}vw`; //Sets the size of each square as a proportion of the viewport width, 40 being the width of the board
+    board.appendChild(currentSquare);
+  }
+  return board;
+}
